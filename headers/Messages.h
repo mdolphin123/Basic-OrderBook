32b415e2b123d996417ad4259d400f597475c090
Jocelyn <jocelyn.wang126@gmail.com>
2026-06-11 01:25:00 -0400

    Added Stock Directory Message Stock

--- headers/Messages.h
+++ headers/Messages.h
@@ -18,4 +18,25 @@ namespace itch {
         char event_code; //market event that happened
     }
 
+    struct StockDirectoryMessage { //Stock directory message
+        char message_type = 'R';
+        uint16_t stock_locate;
+        uint16_t tracking_number;
+        uint64_t timestamp;
+        char stock[8];
+        char market_category;
+        char financial_status_indicator;
+        uint32_t round_lot_size;
+        char round_lots_only;
+        char issue_classification;
+        char issue_sub_type[2];
+        char authenticity;
+        char short_sale_threshold_indicator;
+        char ipo_flag;
+        char luld_ref;
+        char etp_flag;
+        uint32_t etp_leverage_factor;
+        char inverse_indicator;
+};
+
 }
